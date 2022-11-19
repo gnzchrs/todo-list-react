c692a62eb437f2017edeeb880ea5746f8742890b
gnzchrs <christophergonzaga63@gmail.com>
2022-11-19 22:53:50 +0800

    finish signup screen

--- screens/Authentication/Signup.js
+++ screens/Authentication/Signup.js
@@ -1,24 +1,123 @@
-import React from 'react'
-import { View, Text, TextInput, StyleSheet, Image } from 'react-native'
+import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from 'react-native'
+import Button from '../global/Button/Button'
+import { Feather } from 'react-native-vector-icons'
 
 const s = require('../global/globalStyle')
+import Logo from './components/Logo'
 
 const Signup = ({ navigation }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [iconName, setIconName] = useState('eye-off')
+  const [visible, setVisible] = useState(false)
+
+  const visiblePassword = (visible) => {
+    if (visible) {
+      setIconName('eye')
+      setVisible(!visible)
+    }
+    if (!visible) {
+      setIconName('eye-off')
+      setVisible(!visible)
+    }
+  }
+
   return (
     <View style={s.defaultContainer}>
-      <View
-        style={{
-          flex: 2,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Image
-          source={require('../../assets/icon/login.png')}
-          style={{ position: 'absolute' }}
+      <Logo />
+      <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+        <View style={s.inputView}>
+          <TextInput
+            style={[s.input, s.spacing]}
+            placeholder={'Email'}
+            onChangeText={setEmail}
+            value={email}
+          />
+        </View>
+        <View style={s.inputView}>
+          <TextInput
+            style={[s.input, s.spacing]}
+            placeholder={'Full Name'}
+            onChangeText={setEmail}
+            value={email}
+          />
+        </View>
+        <View
+          style={[
+            s.inputView,
+            {
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            },
+          ]}
+        >
+          <TextInput
+            secureTextEntry={visible}
+            placeholderTextColor={'#949494'}
+            style={[s.input, s.spacing, { width: '90%' }]}
+            placeholder={'Password'}
+            onChangeText={setPassword}
+            autoCorrect={false}
+            value={password}
+          />
+          <TouchableOpacity onPress={() => visiblePassword(visible)}>
+            <Feather name={iconName} size={24} color={'#949494'} />
+          </TouchableOpacity>
+        </View>
+        <View
+          style={[
+            s.inputView,
+            {
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            },
+          ]}
+        >
+          <TextInput
+            secureTextEntry={visible}
+            placeholderTextColor={'#949494'}
+            style={[s.input, s.spacing, { width: '90%' }]}
+            placeholder={'Confirm Password'}
+            onChangeText={setPassword}
+            autoCorrect={false}
+            value={password}
+          />
+          <TouchableOpacity onPress={() => visiblePassword(visible)}>
+            <Feather name={iconName} size={24} color={'#949494'} />
+          </TouchableOpacity>
+        </View>
+        <Button
+          type={'primary'}
+          style={{ marginTop: 15 }}
+          text={'SIGN UP'}
+          onPress={() => navigation.navigate('Home')}
         />
+        <View
+          style={{
+            paddingVertical: 20,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text style={{ ...s.input, color: 'rgba(39,39,39,.4)' }}>
+            Have an account?{' '}
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+            <Text style={{ ...s.input, color: '#F79E89' }}>Log in</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={{ flex: 1 }}></View>
     </View>
   )
 }
---
+++ screens/global/TxtInput/TxtInput.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native'
+const s = require('../../global/globalStyle')
+
+const TxtInput = () => {
+  return <View></View>
+}
+
+export default TextInput
+
+const styles = StyleSheet.create({})
